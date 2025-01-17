803d3dd5e15689527cdac28da5ebecc347de1cd7
İsa Eren <isaeren.ie@gmail.com>
2025-01-17 14:53:11 +0100

    Timer added

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -5,7 +5,7 @@ import { SCORE_TEXT_ID } from '../constants.js';
 import { PREVIOUS_QUESTION_BUTTON_ID } from '../constants.js';
 import { PROGRESS_BAR_ID } from '../constants.js';
 import { IMAGES_ID } from '../constants.js';
-
+import { TIMER_ID } from '../constants.js';
 /**
  * Create a full question element
  * @returns {Element}
@@ -21,6 +21,10 @@ export const createQuestionElement = (question) => {
         <div>
             <p>Question No:</p>
             <span id="question-number">1</span>
+        </div>
+        <div>
+            <p>Time:</p>
+            <span id="${TIMER_ID}">60</span>
         </div>
         <div>
             <p>Score:</p>
